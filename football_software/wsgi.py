a03505dded128db65ddac74de4c365a0e028fbd5
Rayan <rayanboukabous74@gmail.com>
2025-07-09 17:37:46 +0100

    final commit before hosting

--- football_software/wsgi.py
+++ football_software/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'football_software.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "football_software.settings")
 
 application = get_wsgi_application()
